8cff31d1a653462404aedca96b3754bdf3bdb039
jiangsheng <sjiang@today36524.com.cn>
2018-11-14 16:46:36 +0800

    jsonEditor 样式修改

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import VJsoneditor from 'v-jsoneditor/src/index'
+import "../static/css/common/index.scss"
+import "../static/css/common/icon/iconfont.css"
 
 Vue.use(VJsoneditor)
 Vue.config.productionTip = false
